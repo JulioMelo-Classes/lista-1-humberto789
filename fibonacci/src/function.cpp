dfb82895ca8e71037082186b2876505c6d7f52a7
humberto789 <humbertovitalino1@gmail.com>
2021-06-15 21:24:48 +0000

    Implementando a função function.cpp do programa Fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -4,6 +4,26 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
 
+    int soma; 
+    int primeira_seq = 1, segunda_seq = 1; 
+    std::vector<unsigned int> vetor;
+
+    if(n>=2 && n!=1){
+      vetor.push_back(1);
+      vetor.push_back(1);
+      if(n>=3){
+        while(true){                          
+          soma = primeira_seq + segunda_seq;                    
+          primeira_seq = segunda_seq;                           
+          segunda_seq = soma;
+          if(segunda_seq<n){
+            vetor.push_back(segunda_seq); 
+          }else{
+            break;
+          }           
+        }
+      }
+    }   
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    return vetor;
 }
