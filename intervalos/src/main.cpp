2de8d7b63dfe5716de370012e9952a5ed577a8ad
humberto789 <56692368+humberto789@users.noreply.github.com>
2021-06-15 16:24:54 -0300

    Implementando o main.cpp do programa Intervalos

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -11,11 +11,64 @@ using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-// Se desejar, crie funções aqui, antes do main().
+bool verificar_intervalo(int inicio, int fim, int numero){
+  bool apareceu = false;
+  for(int i=inicio; i<fim; i++){
+    if(numero==i){
+      apareceu = true;
+    }
+  }
+
+  return apareceu;
+}
+
+double calcular_porcentagem(int numero_total, int numero_parcial ){
+  double resultado = (100*(double)numero_parcial)/(double)numero_total;
+
+  return resultado;
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int numero, qtd_numeros=0;
+    int primeiro_intervalo = 0, segundo_intervalo = 0, terceiro_intervalo = 0, quarto_intervalo = 0, fora = 0;
+    bool apareceu;
+
+    while(cin >> std::ws >> numero){
+      apareceu = false;
+
+      if(verificar_intervalo(0,25, numero)){
+        apareceu = true;
+        primeiro_intervalo++;
+      }
+      else if(verificar_intervalo(25,50, numero)){
+        apareceu = true;
+        segundo_intervalo++;
+      }
+      else if(verificar_intervalo(50,75, numero)){
+        apareceu = true;
+        terceiro_intervalo++;
+      }
+      else if(verificar_intervalo(75,100, numero)){
+        apareceu = true;
+        quarto_intervalo++;
+      }
+
+      if(!apareceu){
+        fora++;
+      }
+      qtd_numeros++;
+    }
+
+    std::cout<<std::setprecision(4)<<calcular_porcentagem(qtd_numeros, primeiro_intervalo)<<endl;
+
+    std::cout<<std::setprecision(4)<<calcular_porcentagem(qtd_numeros, segundo_intervalo)<<endl;
+
+    std::cout<<std::setprecision(4)<<calcular_porcentagem(qtd_numeros, terceiro_intervalo)<<endl;
+
+    std::cout<<std::setprecision(4)<<calcular_porcentagem(qtd_numeros, quarto_intervalo)<<endl;
+    
+    std::cout<<std::setprecision(4)<<calcular_porcentagem(qtd_numeros, fora)<<endl;
 
     return 0;
 }
