d64ba508d1e8a3c04e85bb1274341a989a5d7cc4
humberto789 <humbertovitalino1@gmail.com>
2021-06-16 20:21:52 +0000

    Implementando a função function.cpp do programa Ponto em Retangulo 2

--- ponto_em_retangulo_2/src/function.cpp
+++ ponto_em_retangulo_2/src/function.cpp
@@ -5,7 +5,14 @@
  */
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-    // TODO: Adicione seu código aqui.
+  if(P.x>=IE.x && P.x<=SD.x && P.y>=IE.y && P.y<=SD.y){
 
+    if(P.x==IE.x || P.x==SD.x || P.y==IE.y || P.y==SD.y){
+      return location_t::BORDER;
+    }else{
+      return location_t::INSIDE;
+    }
+  }else{
     return location_t::OUTSIDE;
+  }
 }
