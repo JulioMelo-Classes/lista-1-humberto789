5f2feaa44fdc2f2f24f725bcbbebe1014eb31cbd
humberto789 <56692368+humberto789@users.noreply.github.com>
2021-06-16 11:40:47 -0300

    Implementando o function.cpp do programa Minmax

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -10,10 +10,32 @@
  * @return A pair of indexes to the first smallest and last largest values.
  */
 
-std::pair<int,int> min_max( int V[], size_t n )
+std::pair<int,int> min_max( int V[], std::size_t n )
 {
     // TODO: Adicione aqui sua solução.
 
+    int maior = -1;
+    int menor = -1;
+    int valor_maior=V[0];
+    int valor_menor=V[0];
+    std::pair<int, int> resultado;
+
+    for(int i = 0; i < (int) n; i++){
+      if(V[i] >= valor_maior){
+        maior = i;
+        valor_maior = V[i];
+      }
+    }
+
+    for(int i = n-1; i>=0; i--){
+      if(V[i] <= valor_menor){
+        menor = i;
+        valor_menor = V[i];
+      }
+    }
+
+    resultado = {menor, maior};
+
     // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    return resultado;
 }
